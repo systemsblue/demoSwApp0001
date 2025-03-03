cb2021ebbd1405b015fed73ce3f0fded5bdb3832
Kei <n@iiv.jp>
2025-03-03 10:37:11 +0900

    Initial Commit

---
+++ demoSwApp0001/demoSwApp0001App.swift
@@ -0,0 +1,17 @@
+//
+//  demoSwApp0001App.swift
+//  demoSwApp0001
+//
+//  Created by kzn on 2025/03/03.
+//
+
+import SwiftUI
+
+@main
+struct demoSwApp0001App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ demoSwApp0001Tests/demoSwApp0001Tests.swift
@@ -0,0 +1,16 @@
+//
+//  demoSwApp0001Tests.swift
+//  demoSwApp0001Tests
+//
+//  Created by kzn on 2025/03/03.
+//
+
+import Testing
+
+struct demoSwApp0001Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
